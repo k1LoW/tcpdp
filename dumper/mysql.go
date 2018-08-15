9aaa8d2078a5b0cc589d24cf0dc563e8f7cb5e2c
k1LoW <k1lowxb@gmail.com>
2018-08-15 16:48:59 +0900

    Refactor MySQL dumper

--- dumper/mysql.go
+++ dumper/mysql.go
@@ -1,10 +1,6 @@
 package dumper
 
 import (
-	"bytes"
-	"fmt"
-	"strings"
-
 	"go.uber.org/zap"
 )
 
@@ -18,10 +14,8 @@ func (m *MysqlDumper) Dump(cid string, in []byte) error {
 	if in[4] != 0x03 {
 		return nil
 	}
-	buff := bytes.NewBuffer(in)
-	_, _ = buff.Read(make([]byte, 5))
-	str, _ := buff.ReadString(0x00)
-	query := strings.Trim(str, "\x00")
-	m.Logger.Info(fmt.Sprintf("%s", query), zap.String("cid", cid))
+	n := len(in)
+	query := string(in[5:n])
+	m.Logger.Info(query, zap.String("cid", cid))
 	return nil
 }
