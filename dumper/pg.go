4c7437af8b4c4efbf9c025163b428023fa2162b2
k1LoW <k1lowxb@gmail.com>
2018-09-12 13:04:31 +0900

    Refactor Dumper struct

--- dumper/pg.go
+++ dumper/pg.go
@@ -17,15 +17,22 @@ const (
 
 // PgDumper struct
 type PgDumper struct {
+	name   string
 	logger *zap.Logger
 }
 
 // NewPgDumper returns a PgDumper
 func NewPgDumper() *PgDumper {
 	dumper := &PgDumper{
+		name:   "pg",
 		logger: logger.NewQueryLogger(),
 	}
 	return dumper
+}
+
+// Name return dumper name
+func (p *PgDumper) Name() string {
+	return p.name
 }
 
 // Dump query of PostgreSQL
@@ -35,35 +42,22 @@ func (p *PgDumper) Dump(in []byte, direction Direction, persistent *DumpValues, 
 	}
 
 	// parse StartupMessage to get username, database
-	if len(in) > 9 {
-		splited := bytes.Split(in[8:], []byte{0x00})
-		if len(splited) > 0 && string(splited[0]) == "user" {
-			username := string(splited[1])
-			persistent.Values = append(persistent.Values, DumpValue{
-				Key:   "username",
-				Value: username,
-			})
-			for i, keyOrValue := range splited {
-				if string(keyOrValue) == "database" {
-					persistent.Values = append(persistent.Values, DumpValue{
-						Key:   "database",
-						Value: string(splited[i+1]),
-					})
-				}
-			}
-			return nil
+	pValues := p.ReadPersistentValues(in)
+	if len(pValues) > 0 {
+		for _, kv := range pValues {
+			persistent.Values = append(persistent.Values, kv)
 		}
-	}
-
-	// Query
-	messageType := in[0]
-	if messageType != pgMessageQuery && messageType != pgMessageParse && messageType != pgMessageBind {
 		return nil
 	}
 
-	query := strings.Trim(string(in[5:]), "\x00")
-	fields := []zapcore.Field{
-		zap.String("message_type", string(messageType)),
+	values := p.Read(in)
+	if len(values) == 0 {
+		return nil
+	}
+
+	fields := []zapcore.Field{}
+	for _, kv := range values {
+		fields = append(fields, zap.Any(kv.Key, kv.Value))
 	}
 	for _, kv := range persistent.Values {
 		fields = append(fields, zap.Any(kv.Key, kv.Value))
@@ -72,6 +66,51 @@ func (p *PgDumper) Dump(in []byte, direction Direction, persistent *DumpValues, 
 		fields = append(fields, zap.Any(kv.Key, kv.Value))
 	}
 
-	p.logger.Info(query, fields...)
+	p.logger.Info("-", fields...)
 	return nil
+}
+
+// Read return byte to analyzed string
+func (p *PgDumper) Read(in []byte) []DumpValue {
+	messageType := in[0]
+	if messageType != pgMessageQuery && messageType != pgMessageParse && messageType != pgMessageBind {
+		return []DumpValue{}
+	}
+	query := strings.Trim(string(in[5:]), "\x00")
+	return []DumpValue{
+		DumpValue{
+			Key:   "query",
+			Value: query,
+		},
+		DumpValue{
+			Key:   "message_type",
+			Value: string(messageType),
+		},
+	}
+}
+
+// ReadPersistentValues return persistent value each session
+func (p *PgDumper) ReadPersistentValues(in []byte) []DumpValue {
+	values := []DumpValue{}
+	// parse StartupMessage to get username, database
+	if len(in) < 10 {
+		return values
+	}
+	splited := bytes.Split(in[8:], []byte{0x00})
+	if len(splited) > 0 && string(splited[0]) == "user" {
+		username := string(splited[1])
+		values = append(values, DumpValue{
+			Key:   "username",
+			Value: username,
+		})
+		for i, keyOrValue := range splited {
+			if string(keyOrValue) == "database" {
+				values = append(values, DumpValue{
+					Key:   "database",
+					Value: string(splited[i+1]),
+				})
+			}
+		}
+	}
+	return values
 }
