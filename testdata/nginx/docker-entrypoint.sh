55109d804026a79a9e1a2890a78af49d70ca5ef4
k1LoW <k1lowxb@gmail.com>
2019-02-10 23:46:32 +0900

    Use network_mode

--- testdata/nginx/docker-entrypoint.sh
+++ testdata/nginx/docker-entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 apt-get update
-apt-get install iproute2 -y
+apt-get install iproute2 dnsutils iputils-ping -y
 
 HOST_DOMAIN="host.docker.internal"
 ping -q -c1 $HOST_DOMAIN > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-  ip route
-  HOST_IP=$(ip route | awk 'NR==1 {print $3}')
-  echo "$HOST_IP"
+  # HOST_IP=$(ip route | awk '/default/ { print $3 }' | awk '!seen[$0]++')
+  HOST_IP=127.0.0.1
   echo -e "$HOST_IP\t$HOST_DOMAIN" >> /etc/hosts
   cat /etc/hosts
 fi
