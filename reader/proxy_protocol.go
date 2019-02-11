0ce6d3a19f6aed8767c2af3cdda0c84969fa518e
k1LoW <k1lowxb@gmail.com>
2019-02-11 17:03:07 +0900

    Support part of proxy protocol v2

--- reader/proxy_protocol.go
+++ reader/proxy_protocol.go
@@ -2,6 +2,9 @@ package reader
 
 import (
 	"bytes"
+	"encoding/binary"
+	"errors"
+	"net"
 	"strconv"
 	"strings"
 
@@ -10,7 +13,7 @@ import (
 
 var (
 	v1Prefix = []byte("PROXY")
-	v2Prefix = []byte{0x0d, 0x0a, 0x0d, 0x0a, 0x00, 0x0d, 0x0a, 0x51, 0x55, 0x49, 0x54, 0x0a, 0x02}
+	v2Prefix = []byte{0x0d, 0x0a, 0x0d, 0x0a, 0x00, 0x0d, 0x0a, 0x51, 0x55, 0x49, 0x54, 0x0a}
 )
 
 // ParseProxyProtocolHeader ...
@@ -19,7 +22,7 @@ func ParseProxyProtocolHeader(in []byte) (int, []dumper.DumpValue, error) {
 		return parseProxyProtocolV1Header(in)
 	}
 	if bytes.Index(in, v2Prefix) == 0 {
-
+		return parseProxyProtocolV2Header(in)
 	}
 	return 0, []dumper.DumpValue{}, nil
 }
@@ -53,5 +56,98 @@ func parseProxyProtocolV1Header(in []byte) (int, []dumper.DumpValue, error) {
 }
 
 func parseProxyProtocolV2Header(in []byte) (int, []dumper.DumpValue, error) {
-	return 0, []dumper.DumpValue{}, nil
+	byte13 := in[12]
+	if !(0x20 == byte13 || 0x21 == byte13) {
+		return 0, []dumper.DumpValue{}, errors.New("unexpected values")
+	}
+	// PROXY or LOCAL
+	byte14 := in[13]
+	length := int(binary.BigEndian.Uint16(in[14:16]))
+	idx := 16
+
+	if byte14 == 0x00 {
+		// UNSPEC
+		return idx + length, []dumper.DumpValue{}, errors.New("unexpected values")
+	}
+
+	if 0x10 == byte14&0xf0 {
+		// IPv4: 12byte
+		var (
+			srcAddr net.IP // 4
+			dstAddr net.IP // 4
+			srcPort uint16 // 2
+			dstPort uint16 // 2
+		)
+
+		srcAddr = in[idx : idx+4]
+		idx = idx + 4
+
+		dstAddr = in[idx : idx+4]
+		idx = idx + 4
+
+		srcPort = binary.BigEndian.Uint16(in[idx : idx+2])
+		idx = idx + 2
+
+		dstPort = binary.BigEndian.Uint16(in[idx : idx+2])
+		idx = idx + 2
+		return idx, []dumper.DumpValue{
+			dumper.DumpValue{
+				Key:   "proxy_protocol_src_addr",
+				Value: srcAddr.String(),
+			},
+			dumper.DumpValue{
+				Key:   "proxy_protocol_dst_addr",
+				Value: dstAddr.String(),
+			},
+			dumper.DumpValue{
+				Key:   "proxy_protocol_src_port",
+				Value: srcPort,
+			},
+			dumper.DumpValue{
+				Key:   "proxy_protocol_dst_port",
+				Value: dstPort,
+			},
+		}, nil
+	} else if 0x20 == byte14&0xf0 {
+		// IPv6: 36byte
+		var (
+			srcAddr net.IP // 16
+			dstAddr net.IP // 16
+			srcPort uint16 // 2
+			dstPort uint16 // 2
+		)
+
+		srcAddr = in[idx : idx+16]
+		idx = idx + 16
+
+		dstAddr = in[idx : idx+16]
+		idx = idx + 16
+
+		srcPort = binary.BigEndian.Uint16(in[idx : idx+2])
+		idx = idx + 2
+
+		dstPort = binary.BigEndian.Uint16(in[idx : idx+2])
+		idx = idx + 2
+		return idx, []dumper.DumpValue{
+			dumper.DumpValue{
+				Key:   "proxy_protocol_src_addr",
+				Value: srcAddr.String(),
+			},
+			dumper.DumpValue{
+				Key:   "proxy_protocol_dst_addr",
+				Value: dstAddr.String(),
+			},
+			dumper.DumpValue{
+				Key:   "proxy_protocol_src_port",
+				Value: srcPort,
+			},
+			dumper.DumpValue{
+				Key:   "proxy_protocol_dst_port",
+				Value: dstPort,
+			},
+		}, nil
+	}
+	// AF_UNIX not support
+
+	return idx + length, []dumper.DumpValue{}, errors.New("not supported values")
 }
